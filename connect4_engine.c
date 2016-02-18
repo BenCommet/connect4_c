1b3fb78c3c40c0228db259f750cb52a6d8563148
BenCommet <bencommet@gmail.com>
2016-02-18 00:40:25 -0500

    tests added and passed

--- connect4_engine.c
+++ connect4_engine.c
@@ -7,26 +7,21 @@ int check_horizontal(int num_rows, int num_columns, int length_to_win, int board
 		int player = -1;
 		for(int j = 0; j < num_columns; j++){
 			if(board[i][j] == -1){
-                //printf("1");
 				player = -1;
 				count = 0;
 			}
 			else if(count == 0){
-			   // printf("2");
 				player = board[i][j];
 				count = 1;
 			}
 			else if(board[i][j] == player){
-			    //printf("3");
 				count = count + 1;
 			}
 			else{
-			    //printf("4");
 				count = 1;
 				player = board[i][j];
 			}
 			if(count == length_to_win){
-               // printf("5");
                 return player;
 			}
 		}
@@ -35,23 +30,24 @@ int check_horizontal(int num_rows, int num_columns, int length_to_win, int board
 }
 
 int check_vertical(int num_columns, int num_rows, int length_to_win, int array[num_rows][num_columns]){
-    for(int i = 0; i < num_columns; i++){
+    for(int i = 0; i < num_columns; ++i){
     	int count = 0;
     	int player = -1;
     	for (int j = 0; j < num_rows; j++){
-			if(array[i][j] == -1){
+			if(array[j][i] == -1){
 				player = -1;
+				count = 0;
 			}
 			else if(count == 0){
-				player = array[i][j];
+				player = array[j][i];
 				count = 1;
 			}
-			else if(array[i][j] == player){
+			else if(array[j][i] == player){
 				count = ++count;
 			}
 			else{
 				count = 1;
-				player = array[i][j];
+				player = array[j][i];
 			}
 			if(count == length_to_win){
                 return player;
@@ -61,31 +57,28 @@ int check_vertical(int num_columns, int num_rows, int length_to_win, int array[n
     return -1;
 }
 
-int check_diagonal(int num_columns, int num_rows, int length_to_win, int array[num_rows][num_columns]){
+int check_diagonal_left(int num_columns, int num_rows, int length_to_win, int array[num_rows][num_columns]){
     int maxRows = num_columns + num_rows - 2;
+    int count = 0;
+    int player = -1;
     for (int current_row = 0; current_row < maxRows; current_row++){
         for(int i = 0; i < num_rows; ++i){
-            int player = -1;
-            int count = 0;
             for(int j = 0; j < num_columns; j++){
                 if(i + j - current_row == 0){
-                    if(array[i][j] == -1){
-                        player = -1;
-                    }
-                    else if(count == 0){
-                        player = array[i][j];
-                        count = 1;
-                    }
-                    else if(array[i][j] == player){
-                        count = ++count;
-                    }
-                    else{
-                        count = 1;
-                        player = array[i][j];
-                    }
-                    if(count == length_to_win){
-                        return player;
-                    }
+                	if(array[i][j] == -1){
+                		count = 0;
+                		player = -1;
+                	}
+                	else if(array[i][j] != player){
+                		count = 1;
+                		player = array[i][j];
+                	}
+                	else{
+                		count += 1;
+                	}
+                	if(count == length_to_win){
+                		return player;
+                	}
                 }
             }
         }
@@ -93,7 +86,30 @@ int check_diagonal(int num_columns, int num_rows, int length_to_win, int array[n
     return -1;
 }
 
-
+int check_diagonal_right(int num_columns, int num_rows, int length_to_win, int array[num_rows][num_columns]){
+    int player = -1;
+	for(int i = 0; i < num_rows - (length_to_win - 1); i++){
+		for(int j = 0; j < num_columns - (length_to_win - 1); j++){
+			printf("[%d][%d]%d\n", i, j, array[i][j]);
+			if(array[i][j] != -1){
+				printf("[%d][%d]%d\n", i, j, array[i][j]);
+				int temp = array[i][j];
+				int flag = 1;
+				for(int k = 1; k < length_to_win; k++){
+					if(temp != array[i + k][j + k]){
+						printf("[%d][%d]%d\n", i, j, array[i + k][j + k]);
+						flag = -1;
+					}
+            	}
+            	if(flag == 1){
+            		return temp;
+            	}
+        	}
+		}
+	}
+	return -1;
+	}
+    
 
 int place_token(int player, int column, int num_rows, int num_columns, int board[num_rows][num_columns]){
 	if(column > num_columns){
@@ -118,7 +134,8 @@ int winner(int num_rows, int num_columns, int length_to_win, int array[num_rows]
 	}
 	int horizontal = check_horizontal(num_rows, num_columns, length_to_win, array);
 	int vertical = check_vertical(num_rows, num_columns, length_to_win, array);
-	int diagonal = check_diagonal(num_rows, num_columns, length_to_win, array);
+	int diagonal_left = check_diagonal_left(num_rows, num_columns, length_to_win, array);
+	int diagonal_right = check_diagonal_right(num_rows, num_columns, length_to_win, array);
 
 	if( horizontal != -1){
 		return horizontal;
@@ -126,8 +143,11 @@ int winner(int num_rows, int num_columns, int length_to_win, int array[num_rows]
 	if(vertical != -1){
 		return vertical;
 	}
-	if(diagonal != -1){
-		return diagonal;
+	if(diagonal_left != -1){
+		return diagonal_left;
+	}
+	if(diagonal_right != -1){
+		return diagonal_right;
 	}
 	return -1;
 }
--- connect4_engine.h
+++ connect4_engine.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int check_horizontal(int num_rows, int num_columns, int length_to_win, int board[num_rows][num_columns]);
 int check_vertical(int num_columns, int num_rows, int length_to_win, int array[num_rows][num_columns]);
-int check_diagonal(int num_columns, int num_rows, int length_to_win, int array[num_rows][num_columns]);
+int check_diagonal_left(int num_columns, int num_rows, int length_to_win, int array[num_rows][num_columns]);
+int check_diagonal_right(int num_columns, int num_rows, int length_to_win, int array[num_rows][num_columns]);
 int place_token(int player, int column, int num_rows, int num_columns, int board[num_rows][num_columns]);
 int winner(int num_rows, int num_columns, int length_to_win, int array[num_rows][num_columns]);
